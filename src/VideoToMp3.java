540dc26dfc418f1ddf2c58571000e2914e58011e
apollo78124 <98apollo@naver.com>
2020-05-30 22:39:22 -0700

    Take user input

--- src/VideoToMp3.java
+++ src/VideoToMp3.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class VideoToMp3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int songNumber;
+		String[] links;
+		Scanner scn = new Scanner(System.in);
+		boolean pageTurn = false;
+		
+		System.out.println("How many songs?");
+		songNumber = scn.nextInt();
+		links = new String[songNumber];
+		
+		for (int i = 0; i < songNumber; i++) {
+			System.out.println("Paste the link");
+			links[i] = scn.next();
+		}
 	}
 
 }

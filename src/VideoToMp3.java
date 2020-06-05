a8f4d77354eff811e273d7fb5825990e63b9ffbb
apollo78124 <98apollo@naver.com>
2020-06-05 01:14:28 -0700

    Song download works

--- src/VideoToMp3.java
+++ src/VideoToMp3.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class VideoToMp3 {
 
 	public static void main(String[] args) {
@@ -16,6 +21,35 @@ public class VideoToMp3 {
 		for (int i = 0; i < songNumber; i++) {
 			System.out.println("Paste the link");
 			links[i] = scn.next();
+		}
+		
+		for (int i = 0; i < links.length; i++) {
+			try {
+		         
+		    	  System.setProperty("webdriver.chrome.driver", 
+							"chromedriver.exe");
+					WebDriver driver = new ChromeDriver();;
+					try {
+						
+						driver.get("https://ytmp3.cc/");
+						WebElement url = driver.findElement(By.id("input"));
+						url.click();
+						url.sendKeys(links[i]);
+						WebElement convertForm = driver.findElement(By.id("submit"));
+						convertForm.submit();
+						Thread.sleep(1500);
+						//WebElement download = driver.findElement(By.id("download"));
+						//download.click();
+						
+					} catch (Exception e) {
+						System.out.println("Thread " +  links[i] + " interrupted.");
+					}
+					
+		    	  
+		      } catch (Exception e) {
+		         System.out.println("Thread " +  links[i] + " interrupted.");
+		      }
+		      System.out.println("Thread " +  links[i] + " exiting.");
 		}
 	}
 
